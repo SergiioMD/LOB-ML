1cab7fa2d12d57953fa989ca38d1e7a9ff09e972
SergiioMD <sergimonmon1345@gmail.com>
2025-09-10 14:10:48 +0200

    Update orderbook.py

--- simulator/orderbook.py
+++ simulator/orderbook.py
@@ -14,22 +14,34 @@ class Order:
 @dataclass
 class Trade:
     time : float
-    buy_order_id: Optional[int]
-    sell_order_id: Optional[int]
+    taker_order_id: Optional[int]
+    maker_order_id: Optional[int]
     price: float
     qty: float
-
+    taker_side: str
+    
 class OrderBook:
     def __init__(self):
         self.bids = {}
         self.asks = {}
         self.trades = []
+        self.next_order_id = 1
 
     def add_limit_order(self, order: Order):
+       
+        order = Order(
+            id = self.next_order_id,
+            side = side,
+            qty = qty,
+            price = price,
+            timestamp = timestamp
+        )
+        self.next_order_id += 1
+        
         book = self.bids if order.side == "buy" else self.asks 
-        if order.price not in book:
-            book[order.price] = deque()
-        book[order.price].append(order)
+        if price not in book:
+            book[price] = deque()
+        book[price].append(order)
   
     def best_bid(self):
         return max(self.bids.keys()) if self.bids else None
@@ -47,40 +59,67 @@ class OrderBook:
             return self.best_ask() - self.best_bid()
         return None
   
-    def market_order(self, side: str , qty: float , timestamp: float):
+    def market_order(self, side: str, qty: float, timestamp: float):
+    
+        order = Order(
+            id = self.next_order_id,
+            side = side,
+            qty = qty,
+            price = None,  # market order no tiene precio
+            timestamp = timestamp
+        )
+        self.next_order_id += 1
+    
         book = self.asks if side == "buy" else self.bids
-        while qty > 0 and book:
+    
+        while order.qty > 0 and book:
             best_price = min(book.keys()) if side == "buy" else max(book.keys())
             level = book[best_price]
-            best_order = level[0]
-
-            trading_qty = min(qty, best_order.qty)
-            qty -= trading_qty
+            best_order = level[0]  # maker
+    
+            trading_qty = min(order.qty, best_order.qty)
+    
+            order.qty -= trading_qty
             best_order.qty -= trading_qty
-
+    
             self.trades.append(
                 Trade(
-                    time=timestamp,
-                    buy_order_id=best_order.id if side == "sell" else None,
-                    sell_order_id=best_order.id if side == "buy" else None,
-                    price=best_price,
-                    qty=trading_qty
+                    time = timestamp,
+                    price = best_price,
+                    qty = trading_qty,
+                    taker_order_id = order.id,
+                    maker_order_id = best_order.id,
+                    taker_side = side
                 )
             )
-
+    
             if best_order.qty == 0:
                 level.popleft()
                 if not level:
                     del book[best_price]
 
-    
-  
-  
+    def cancel_order(self,order_id: int) -> bool:
+        for book in [self.bids, self.asks]:
+            for price, level in list(book.items()):
+                for order in list(level):
+                    if order.id == order_id:
+                        level.remove(order)
+                        if not level:
+                            del book[price]
+                        return True
+        return False
 
+    def depth(self, n:int):
+        result = { "bids":[] , "asks":[]}
 
-
-    
-
+        for price in sorted(self.bids.keys(), reverse= True)[:n]:
+            qty_total1 = sum(order.qty for order in self.bids[price])
+            result["bids"].append((price, qty_total1))
+        for price in sorted(self.asks.keys())[:n]:
+            qty_total2 = sum(order.qty for order in self.asks[price])
+            result["asks"].append((price, qty_total2))
+        print(result)
+        
 
 
   
